b6182ce6743cb72b58d743ffb8280a48fc1f6afb
Adam Rezner <adam.rezner95@gmail.com>
2017-04-19 14:55:55 +0200

    state model change

--- src/application/ModelStanu.java
+++ src/application/ModelStanu.java
@@ -4,11 +4,16 @@ public class ModelStanu {
 	private String nazwa;
 	private double Base_sales_tax;
 	private double Total_max_surtax;
-	
-	ModelStanu(String nazwa,double Base_sales_tax, double Total_max_surtax){
+	private double productTypeTax[] = new double[5];
+	ModelStanu(String nazwa,double Base_sales_tax, double Total_max_surtax, double t1, double t2, double t3, double t4, double t5){
 		this.nazwa=nazwa;
 		this.Base_sales_tax=Base_sales_tax;
 		this.Total_max_surtax=Total_max_surtax;
+		this.productTypeTax[0] = t1;
+		this.productTypeTax[1] = t2;
+		this.productTypeTax[2] = t3;
+		this.productTypeTax[3] = t4;
+		this.productTypeTax[4] = t5;
 	}
 
 	public String getNazwa() {
@@ -34,5 +39,7 @@ public class ModelStanu {
 	public void setTotal_max_surtax(double total_max_surtax) {
 		Total_max_surtax = total_max_surtax;
 	}
-
+	public double calculateTax(int productType){
+		return (Base_sales_tax+productTypeTax[productType]);
+	}
 }
--- src/application/Product.java
+++ src/application/Product.java
@@ -3,30 +3,14 @@ package application;
 public class Product {
 	private String name;
 	private float price;
-	private ProductType productType;
-	private enum ProductType{
-		NoCategory(0),
-		Groceries(1),
-		PreparedFood(2),
-		PrescriptionDrug(3),
-		NonPrescriptionDrug(4),
-		Clothing(5),
-		Intangibles(6);
-		private int value;
-		private ProductType(int value) {
-			this.value = value;
-		}
-		public int getProductValue() {
-			return value;
-		}
-	}
+	
+	
 	public Product(){
-		this("",0.0f,null);
+		this("",0.0f);
 	}
-	public Product(String name, float price, ProductType category){
+	public Product(String name, float price){
 		this.name = name;
 		this.price = price;
-		this.productType = category;
 	}
 	public String getName() {
 		return name;
