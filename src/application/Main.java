88f1bbba65da8c9f87ac031052c1d372b686bc9a
milosz.szyrner <milosz.szyrner@gmail.com>
2017-06-07 16:06:54 +0200

    Change

--- src/application/Main.java
+++ src/application/Main.java
@@ -22,6 +22,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			createCategory();
+			 
 			this.primaryStage = primaryStage;
 			FXMLLoader loader = new FXMLLoader();
 			loader.setLocation(Main.class.getResource("Makieta.fxml"));
